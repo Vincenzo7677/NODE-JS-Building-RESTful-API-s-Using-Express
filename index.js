59ca0eed4f665f7ba3571c76a6146841d1a8f87c
vincenzoschiavone <vincenzo_schiavone@icloud.com>
2020-04-27 17:22:27 +0200

    Added delete handler

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express')
+const app = express()
 const path = require('path')
-const hoganMiddleware = require('hogan-middleware')
+const indexRouter = require('./routes/index')
 
-const app = express();
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'mustache')
-app.engine('mustache', hoganMiddleware.__express)
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.json())
-
-const indexRouter = require('./routes/index');
 app.use('/', indexRouter)
 
-app.listen(5000)
-console.log('The server is running on port 5000...')
+
+
+
+app.listen(3000)
+console.log('Server is running on Port 3000...')
--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,85 @@
+const Joi = require('joi')
 const express = require('express')
-const Joi = require('@hapi/joi')
-
 const router = express.Router()
+router.use(express.json())
 
-router.use(express.json());
 
-const users = [{
+const Users = [{
     id: 1,
-    user: 'Vincenzo Schiavone',
+    name: "Vincenzo",
     age: 29
 },
 {
     id: 2,
-    user: 'Bea Schiavone',
-    age: 26
+    name: "Bea",
+    age: 20
 }];
 
-router.get('/', (req, res) => {
-    res.send('Hello from the Router folder!')
+
+router.get('/', (req, res, next) => {
+    res.send('Hello World from Router index.js')
 })
 
-router.get('/home', (req, res) => {
-    res.render('home', null)
-})
-
-router.get('/users', (req, res) => {
-    res.json(users)
-})
-
-router.get('/users/:id', (req, res) => {
-    const user = users.find(c => c.id === parseInt(req.params.id));
-    if (!user) res.status(404).send("The User with this given ID was not found.");
+router.get('/api/users/:id', (req, res, next) => {
+    const user = Users.find(c => c.id == parseInt(req.params.id));
+    if(!user) return res.status(404).send("The user with the given ID was not found.")
     res.send(user)
 })
 
-router.post('/users', (req, res) => {
-    const { error } = validateUser(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+
+router.get('/api/users', (req, res, next) => {
+    res.json(Users)
+
+})
+
+
+router.put('/api/users/:id', (req,res,next) => {
+    const user = Users.find(c => c.id == parseInt(req.params.id));
+    if(!user) return res.status(404).send("The user with the given ID was not found.")
+     
+    const schema = {
+        name: Joi.string().min(3).required(),
+        age: Joi.number().min(18).max(99).required()
+    };
+    //Create validation put in to result 
+    const result = Joi.validate(req.body, schema);
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+        
+    user.name = req.body.name;
+    user.age = req.body.age;
+    res.send(user)
+});
+
+router.delete('/api/users/:id', (req,res,next) => {
+    const user = Users.find(c => c.id == parseInt(req.params.id));
+    if(!user) return res.status(404).send("The user with the given ID was not found.")
+
+    const index = Users.indexOf(user);
+    Users.splice(index, 1);
+    res.send(user)
+}) 
+
+router.post('/api/users/', (req, res, next) => {
+    //Create Joi schema object
+    const schema = {
+        name: Joi.string().min(3).required(),
+        age: Joi.number().min(18).max(99).required()
+    };
+    //Create validation put in to result 
+    const result = Joi.validate(req.body, schema);
+    //If there is an error send 400 status, with a details message
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+    //Create new user check if
     const user = {
-        id: users.length + 1,
-        user: req.body.user,
+        id: Users.length + 1,
+        name: req.body.name,
         age: req.body.age
     };
-    users.push(user);
-    res.send(user);
-});
 
-router.put('/users/:id', (req, res) => {
-    const user = users.find(c => c.id === parseInt(req.params.id));
-    if (!user) res.status(404).send("The User with this given ID was not found.");
-
-    user.user = req.body.user;
-    user.age = req.body.age;
-
+    Users.push(user)
     res.send(user)
-});
 
-function validateUser(user) {
-    const schema = Joi.object({
-        user: Joi.string().required(),
-        age: Joi.number().integer().required()
-    });
-    return schema.validate(user,{});
-}
+})
 
-module.exports = router;
+
+module.exports = router
